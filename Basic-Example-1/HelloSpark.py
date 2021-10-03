081215face5576a440e04bea261221599a8f4ddf
Amit Patel <amitptl.in@gmail.com>
2021-10-03 07:18:47 +0400

    config details

--- Basic-Example-1/HelloSpark.py
+++ Basic-Example-1/HelloSpark.py
@@ -1,18 +1,23 @@
-from pyspark.sql import SparkSession
+from pyspark.sql.session import SparkSession
+
 from lib.logger import Log4J
+from lib.utils import get_spark_app_config
 
 if __name__ == "__main__":
-    # spark session is a driver manager
-    spark = SparkSession.builder\
-        .appName("Basic-Example-1")\
-        .master("local[3]")\
+    # set config
+    conf = get_spark_app_config()
+    spark = SparkSession.builder \
+        .config(conf=conf) \
         .getOrCreate()
 
     logger = Log4J(spark)
 
-    logger.info("Starting Basic-Example-1")
+    logger.info("Starting Basic-Example")
 
+    # print all configuration
+    conf_out = spark.sparkContext.getConf()
+    logger.info(conf_out.toDebugString())
     # do the business logic
-    logger.info("Finished Basic-Example-1")
+    logger.info("Finished Basic-Example")
 
     spark.stop()
